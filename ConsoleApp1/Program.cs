2a7c22b0d02717ed0a7e13df7fe7308122513f6e
_ Trentini _ <pietrotrentini5@gmail.com>
2020-12-02 12:33:27 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Funzioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Funzioni.InserisciNumeroInt();
+            int b = Funzioni.InserisciNumeroInt();
+            Funzioni.ContaPariDispari(a, b);
+        }
+    }
+}
---
+++ Funzioni/Funzioni.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Funzioni
+{
+    public class Funzioni
+    {
+        public static int InserisciNumeroInt()
+        {
+            int n;
+            do
+            {
+                Console.Write("inserisci un numero intero: ");
+                n = int.Parse(Console.ReadLine());
+            } while (n < 0);
+            return n;
+        }
+        public static void ContaPariDispari(int a, int b)
+        {
+            int contaPari = 0;
+            int contaDispari = 0;
+            int max = Math.Max(a, b);
+            int min = Math.Min(a, b);
+            for (int i = min; i <= max; i++)
+            {
+                if (i%2==0)
+                {
+                    contaPari++;
+                    Console.WriteLine($"{i} pari");
+                }
+                else
+                {
+                    contaDispari++;
+                    Console.WriteLine($"{i} Dispari");
+                }
+            }
+            Console.WriteLine($"I positivi sono:{contaPari}\n" +
+                $"I negativi sono:{contaDispari}");
+            
+        }
+    }
+}
